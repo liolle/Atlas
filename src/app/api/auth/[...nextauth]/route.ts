f65805497d90f9e97ac8538f35244fede7bc5a40
liolle <laurentlionel20@hotmail.com>
2023-10-02 23:44:53 +0200

    [Fix] moving context out of /app

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,5 @@
 import NextAuth from "next-auth";
-import { authOptions } from "@/src/app/api/auth/options";
+import { authOptions } from "@/src/lib/utils";
 
 const handler = NextAuth(authOptions);
 
