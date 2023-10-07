cacd760577ed4c5eb302656e872a9ce3839b21c8
liolle <laurentlionel20@hotmail.com>
2023-10-07 04:57:16 +0200

    Update signIn  & signup page

--- src/app/api/auth/signin/page.tsx
+++ src/app/api/auth/signin/page.tsx
@@ -1,7 +1,16 @@
+import { FormSignIn } from "@/src/components/client/Forms/formSignIn";
+import Link from "next/link";
 import React from "react";
 
-export default function Login() {
+export default function SignIn() {
     return (
-        <div className="bg-main-background flex h-screen items-center justify-center bg-cover p-10"></div>
+        <div className="flex h-screen flex-col items-center justify-center bg-neutral-500 bg-cover ">
+            <span>
+                New user ?{" "}
+                <Link href="/api/auth/signup">Sign up for an account</Link>
+            </span>
+
+            <FormSignIn />
+        </div>
     );
 }
