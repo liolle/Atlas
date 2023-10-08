a136d546b7a36af9fec94ccb421d3d96c662748d
liolle <laurentlionel20@hotmail.com>
2023-10-08 07:14:23 +0200

    update pages

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,5 +1,11 @@
+import SignOutButton from "@/src/components/client/Buttons/signout";
 import React from "react";
 
-export default function Login() {
-    return <div></div>;
+export default async function Home() {
+    return (
+        <main className="flex min-h-screen items-center justify-center ">
+            <span>Home page</span>
+            <SignOutButton />
+        </main>
+    );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
 import React from "react";
+import NavButton from "../components/client/Buttons/NavigationButton";
 
 export default function Main() {
-    return <main className="flex min-h-screen ">Hello</main>;
+    return (
+        <main className="flex min-h-screen items-center justify-center ">
+            Landing page
+            <NavButton route="/home" text="Home">
+                Home
+            </NavButton>
+        </main>
+    );
 }
