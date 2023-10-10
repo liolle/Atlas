9a3ee94a516e17eae2a3ed3196eef68ae2d923aa
liolle <laurentlionel20@hotmail.com>
2023-10-11 01:52:43 +0200

    [Update] utils functions

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,30 @@
-import { type ClassValue, clsx } from "clsx";
+import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { ACCOUNT_ID_LEN, USER_ID_LEN } from "../types";
+import { cryptoGenerate } from "./generator";
+import bcrypt from "bcryptjs";
 
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
 }
+
+export const generateUserID = async () => {
+    return cryptoGenerate(USER_ID_LEN);
+};
+
+export const generateAccountID = async () => {
+    return cryptoGenerate(ACCOUNT_ID_LEN);
+};
+
+export const hashPassword = async (password: string) => {
+    return bcrypt.hashSync(password);
+};
+
+export const comparePassword = async (
+    password: string,
+    hashedPassword: string
+) => {
+    return bcrypt.compareSync(password, hashedPassword);
+};
+
+export const API_URL = `${process.env.API_URL || "http://localhost:3000"}`;
