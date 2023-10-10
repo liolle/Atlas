51066e7d317a2a5dde517b4253fc7c983d57c606
liolle <laurentlionel20@hotmail.com>
2023-10-11 01:49:35 +0200

    [Update] drizzle setup

---
+++ src/db/index.ts
@@ -0,0 +1,13 @@
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+import * as schema from "./schema";
+
+// const connectionString =
+//     process.env.DATABASE_URL ||
+//     "postgres:/postgres:postgres@localhost:5432/atlas";
+
+const connectionString = "postgres://postgres:postgres@localhost:5432/atlas";
+
+const client = postgres(connectionString);
+
+export const dzClient = drizzle(client, { schema: schema });
