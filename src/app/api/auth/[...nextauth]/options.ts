fa4863eb770347f70fed037fe9719f4d9de90bb6
liolle <laurentlionel20@hotmail.com>
2023-10-17 14:18:30 +0200

    [Update] newt-auth

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,121 +1,54 @@
 import type { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import CredentialsProvider from "next-auth/providers/credentials";
 import DiscordProvider from "next-auth/providers/discord";
-import { APIContent, LoginReturnType } from "@/src/types";
-import { API_URL } from "@/src/lib/utils";
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import EmailProvider from "next-auth/providers/email";
+import { dzClient } from "@/src/db";
+import { sendVerificationRequest } from "@/src/services/nodemailer";
+import { generateVToken } from "@/src/lib/utils";
 
 export const authOptions: NextAuthOptions = {
     secret: process.env.NEXTAUTH_SECRET,
-    session: { strategy: "jwt" },
+    session: {
+        strategy: "jwt",
+        maxAge: 30 * 24 * 60 * 60, // 30 days
+        updateAge: 24 * 60 * 60 // 24 hours
+    },
+    adapter: DrizzleAdapter(dzClient),
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID as string,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-            name: "google"
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
         }),
         DiscordProvider({
             clientId: process.env.DISCORD_CLIENT_ID as string,
             clientSecret: process.env.DISCORD_CLIENT_SECRET as string
         }),
-        CredentialsProvider({
-            name: "Credentials",
-            credentials: {
-                email: { label: "Email", type: "email", placeholder: "Email" },
-                password: {
-                    label: "Password",
-                    type: "password",
-                    placeholder: "Password"
+        EmailProvider({
+            server: {
+                host: process.env.EMAIL_SERVER_HOST,
+                port: process.env.EMAIL_SERVER_PORT,
+                auth: {
+                    user: process.env.EMAIL_SERVER_USER,
+                    pass: process.env.EMAIL_SERVER_PASSWORD
                 }
             },
-            async authorize(credentials) {
-                if (
-                    !credentials ||
-                    !credentials.email ||
-                    !credentials.password
-                ) {
-                    return null;
-                }
+            from: process.env.EMAIL_FROM,
 
-                try {
-                    const response = await fetch(
-                        `${API_URL}/api/accounts/login`,
-                        {
-                            method: "POST",
-                            body: JSON.stringify({
-                                email: credentials.email,
-                                password: credentials.password
-                            })
-                        }
-                    );
+            async generateVerificationToken() {
+                const vToken = await generateVToken();
+                return vToken;
+            },
 
-                    const data: APIContent = await response.json();
-
-                    const { error, content } = data as {
-                        error: string;
-                        content: LoginReturnType;
-                    };
-
-                    if (!response.ok || !content) {
-                        console.log(error);
-                        return null;
-                    }
-
-                    return {
-                        id: content.user_id,
-                        email: credentials.email,
-                        image: content.picture,
-                        role: content.role,
-                        created_at: content.user_created_at,
-                        name: content.username
-                    };
-                } catch (error) {
-                    console.log(error);
-                    return null;
-                }
-            }
+            sendVerificationRequest(param) {
+                sendVerificationRequest(param);
+            },
+            maxAge: 60 * 5
         })
     ],
     callbacks: {
-        async session({ session, token }) {
-            /*TODO*/
-            // normalize the return value
-            const sanitizedToken = Object.keys(token).reduce((p, c) => {
-                if (
-                    c !== "iat" &&
-                    c !== "exp" &&
-                    c !== "jti" &&
-                    c !== "apiToken"
-                ) {
-                    return { ...p, [c]: token[c] };
-                } else {
-                    return p;
-                }
-            }, {});
-            return {
-                ...session,
-                user: sanitizedToken,
-                apiToken: token.apiToken
-            };
-        },
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        async signIn({ user, account, profile, email, credentials }) {
-            //TODO
-            // check email verified here
-            // sync with db here
-
-            if (account?.provider == "google") {
-                return true;
-            }
-
-            if (account?.provider == "discord") {
-                return true;
-            }
-
-            return true;
-        },
-        async jwt({ token }) {
-            return token;
+        async session({ session }) {
+            return session;
         }
     }
 };
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,17 @@
-import withAuth from "next-auth/middleware";
+import { withAuth } from "next-auth/middleware";
 
-export default withAuth(function middleware() {}, {
-    callbacks: {
-        authorized: ({ token }) => {
-            if (!token) return false;
-
-            return true;
+export default withAuth(
+    // `withAuth` augments your `Request` with the user's token.
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    function middleware(req) {},
+    {
+        callbacks: {
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
+            authorized: async ({ req, token }) => {
+                return !!token;
+            }
         }
     }
-});
+);
 
 export const config = { matcher: ["/home"] };
