a1d0fcf535e6fa200c47238abaf778c597351e0b
liolle <laurentlionel20@hotmail.com>
2023-10-18 07:23:50 +0200

    [Update] home - v0.1

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,11 +1,14 @@
-import SignOutButton from "@/src/components/client/Buttons/signout";
+// import SignOutButton from "@/src/components/client/Buttons/signout";
+import NavBar from "@/src/components/server/navbar";
 import React from "react";
 
 export default async function Home() {
     return (
-        <main className="flex min-h-screen items-center justify-center gap-2 bg-bgc text-content">
-            <span>Home page</span>
-            <SignOutButton />
+        <main className="flex h-screen items-center justify-center gap-2 bg-bgc text-content">
+            <nav className=" @container flex h-full flex-[0_1_20%] justify-end border-r-2 border-accent-2">
+                <NavBar />
+            </nav>
+            <section className=" flex-1"></section>
         </main>
     );
 }
