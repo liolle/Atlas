7ee6437e43d781ad673e9879e2a72803c74bffbf
liolle <laurentlionel20@hotmail.com>
2023-10-19 10:07:49 +0200

    [Update] middleware - adding page to header

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,22 @@
 import { withAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
 
 export default withAuth(
     // `withAuth` augments your `Request` with the user's token.
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    function middleware(req) {},
+    function middleware(req) {
+        // req.nextUrl.searchParams.currentPage = "test";
+        const requestHeaders = new Headers(req.headers);
+        const page = req.nextUrl.pathname?.split(/[/?]/)[1];
+        requestHeaders.set("x-page", page);
+
+        return NextResponse.next({
+            request: {
+                // Apply new request headers
+                headers: requestHeaders
+            }
+        });
+    },
     {
         callbacks: {
             // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -14,4 +27,4 @@ export default withAuth(
     }
 );
 
-export const config = { matcher: ["/home"] };
+export const config = { matcher: ["/home", "/profile"] };
