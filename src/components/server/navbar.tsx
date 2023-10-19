afcd8510fd0d3b25a2665a0c68ec9fce6b31ce69
liolle <laurentlionel20@hotmail.com>
2023-10-19 10:09:55 +0200

    [Add] profile page -

--- src/components/server/navbar.tsx
+++ src/components/server/navbar.tsx
@@ -5,8 +5,11 @@ import { getServerSession } from "next-auth";
 // import Image from "next/image";
 import AvatarHome from "../client/Buttons/AvatarHome";
 
+import { headers } from "next/headers";
+
 const NavBar = async () => {
     const session = await getServerSession();
+
     return (
         <div className=" flex h-full w-fit flex-col items-center  justify-between gap-4 py-6 ">
             <div className="flex  h-full flex-col gap-4 @[250px]:w-[250px]  ">
@@ -16,7 +19,8 @@ const NavBar = async () => {
                 >
                     <span>LOGO</span>
                 </Link>
-                <NavigationItem />
+                <NavigationItem variant="home" />
+                <NavigationItem variant="profile" />
             </div>
             <AvatarHome
                 image={
@@ -31,27 +35,47 @@ const NavBar = async () => {
 
 interface NavItemItemProps {
     variant?: NavigationVariant;
-    isActive?: boolean;
 }
 
-const NavigationItem = ({
-    variant = "home",
-    isActive = true
-}: NavItemItemProps) => {
+const NavigationItem = ({ variant = "home" }: NavItemItemProps) => {
+    const headersList = headers();
+    const activePage = headersList.get("x-page");
     switch (variant) {
         case "home":
             return (
                 <Link
-                    className=" flex w-fit justify-center gap-2 rounded-full p-2 text-xl text-content hover:bg-accent-1"
+                    className={`flex w-fit justify-center gap-2 rounded-full p-2 text-xl text-content ${"hover:text-accent-1"}`}
                     href="/home"
                 >
-                    <HomeSVG isActive={isActive} />
-                    <span className=" hidden  @[250px]:block">Home</span>
+                    <HomeSVG isActive={activePage == variant} />
+                    <span
+                        className={`hidden  @[250px]:block ${
+                            activePage == variant && "font-bold text-accent-1"
+                        }`}
+                    >
+                        Home
+                    </span>
+                </Link>
+            );
+        case "profile":
+            return (
+                <Link
+                    className={`flex w-fit justify-center gap-2 rounded-full p-2 text-xl text-content ${"hover:text-accent-1"}`}
+                    href="/profile"
+                >
+                    <ProfileSVG isActive={activePage == variant} />
+                    <span
+                        className={`hidden  @[250px]:block ${
+                            activePage == variant && "font-bold text-accent-1"
+                        }`}
+                    >
+                        Profile
+                    </span>
                 </Link>
             );
 
         default:
-            return <>Should not get here</>;
+            return <></>;
     }
 };
 
@@ -67,21 +91,62 @@ const HomeSVG = ({ isActive = false, size = 30 }: SVGIcon) => {
                 width={size}
                 height={size}
                 viewBox={`0 0 ${size} ${size}`}
-                className={` left-[50%] top-[50%]  ${
-                    isActive
-                        ? " fill-current text-transparent"
-                        : "  text-transparent"
+                className={`fill-current ${
+                    isActive ? "text-accent-1" : "text-content"
                 }`}
                 xmlns="http://www.w3.org/2000/svg"
             >
                 <path
                     d="M12 2L3 9V20C3 20.5304 3.21071 21.0391 3.58579 21.4142C3.96086 21.7893 4.46957 22 5 22H9V12H15V22H19C19 22 20.0391 21.7893 20.4142 21.4142C20.7893 21.0391 21 20.5304 21 20V9L12 2Z"
-                    className="stroke-current text-content"
+                    className={`stroke-current ${
+                        isActive ? "text-accent-1" : "text-content"
+                    }`}
                     transform={`scale(${size / 23})`}
                     strokeWidth="2"
                     strokeLinecap="round"
                     strokeLinejoin="round"
                 />
+            </svg>
+        </div>
+    );
+};
+
+const ProfileSVG = ({ isActive = false, size = 30 }: SVGIcon) => {
+    return (
+        <div className=" relative left-[-1px] top-[-3px] flex-1">
+            <svg
+                width={size}
+                height={size}
+                viewBox={`0 0 ${size} ${size}`}
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+            >
+                <g
+                    clipPath="url(#a)"
+                    className={`stroke-current ${
+                        isActive ? "text-accent-1" : "text-content"
+                    }`}
+                    strokeMiterlimit="10"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                >
+                    <path
+                        d="M15 16c3.866 0 7-3.358 7-7.5C22 4.358 18.866 1 15 1S8 4.358 8 8.5c0 4.142 3.134 7.5 7 7.5Z"
+                        className={`fill-current ${
+                            isActive ? "text-accent-1" : "text-content"
+                        }`}
+                        transform={`scale(${size / 30})`}
+                    />
+                    <path
+                        d="M22.847 16c4.09 2.338 6.868 6.466 7.152 11.216.016.429-.347.784-.805.784H.806c-.458 0-.821-.355-.806-.784.285-4.72 3.048-8.834 7.09-11.172L12.368 16l2.632.76 2.63-.76h5.216Z"
+                        className={` left-[50%] top-[50%]  ${
+                            isActive
+                                ? "fill-current text-accent-1"
+                                : "fill-current text-content"
+                        }`}
+                        transform={`scale(${size / 30})`}
+                    />
+                </g>
             </svg>
         </div>
     );
