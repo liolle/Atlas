b0e218841fdb61878fc797583999024d46f096c5
liolle <laurentlionel20@hotmail.com>
2023-10-24 08:46:57 +0200

    [Update] toast - does not change toast color yet

--- src/services/toast/toast.tsx
+++ src/services/toast/toast.tsx
@@ -2,8 +2,21 @@ import toast from "react-hot-toast";
 import MessageToast from "./components/MessageToast";
 import React from "react";
 
-export const ToastMessage = (message: string) =>
+export const ToastMessage = (
+    message: string,
+    options?: {
+        variant?: "error" | "success" | "default";
+    }
+) =>
     toast.custom((t) => <MessageToast t={t} message={message} />, {
         duration: 2000,
-        position: "bottom-right"
+        position: "bottom-right",
+        className: `${
+            options &&
+            (options.variant == "success"
+                ? " text-message-success"
+                : options.variant == "error"
+                ? "text-message-error"
+                : "")
+        }`
     });
