a7ae6968b4d5382bef9893e6cabf4bb6a8a8a3c9
liolle <laurentlionel20@hotmail.com>
2023-10-25 05:42:53 +0200

    [Update] CardProfile - passing the all user and actions as props

--- src/app/users/[name]/layout.tsx
+++ src/app/users/[name]/layout.tsx
@@ -1,7 +1,7 @@
 import CardProfile from "@/src/components/client/cards/cardProfile";
 import BaseLayout from "@/src/components/server/BaseLayout";
 import { GetUsers } from "@/src/db/portal";
-import { FollowType, isBaseError } from "@/src/types";
+import { isBaseError } from "@/src/types";
 import { getServerSession } from "next-auth";
 import React from "react";
 export default async function Layout({
@@ -11,7 +11,9 @@ export default async function Layout({
     children: React.ReactNode;
     params: { name: string; page: string };
 }) {
+    const session = await getServerSession();
     const user = await GetUsers({
+        self: session?.user?.name || " ",
         field: "name",
         value: params.name
     });
@@ -26,17 +28,11 @@ export default async function Layout({
         );
     }
 
-    const session = await getServerSession();
-    const profile: FollowType = {
-        name: user[0].data.name || "",
-        image: user[0].data.image || ""
-    };
-
     return (
         <BaseLayout>
             <section className=" flex h-full flex-col ">
                 <div className=" flex h-60 w-full items-center justify-center border-b-2 border-accent-2 ">
-                    <CardProfile profile={profile} session={session} />
+                    <CardProfile user={user[0]} session={session} />
                 </div>
 
                 <main className=" flex w-full flex-1 flex-col">{children}</main>
--- src/components/client/cards/cardProfile.tsx
+++ src/components/client/cards/cardProfile.tsx
@@ -1,58 +1,84 @@
 "use client";
 import React, { MouseEvent } from "react";
-import { FollowType } from "@/src/types";
+import { FollowType, LinkAction } from "@/src/types";
 import { Button } from "../../ui/button";
 import { Separator } from "../../ui/separator";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { Session } from "next-auth";
 import Image from "next/image";
+import { ToastMessage } from "@/src/services/toast/toast";
+
+interface CardUserFollowType {
+    data: FollowType;
+    actions: LinkAction[];
+}
 
 const CardProfile = ({
-    profile,
+    user,
     session
 }: {
-    profile: FollowType;
+    user: CardUserFollowType;
     session: Session | null;
 }) => {
     const router = useRouter();
-    const isOWner = (session && profile.name == session.user?.name) ?? false;
-    const handleClick = (e: MouseEvent<HTMLButtonElement>) => {
+    const isOWner = (session && user.data.name == session.user?.name) ?? false;
+    const handleEdit = (e: MouseEvent<HTMLButtonElement>) => {
         e.preventDefault();
         router.push("/account");
     };
 
-    const handleFollow = (e: MouseEvent<HTMLButtonElement>) => {
+    const handleFollow = async (e: MouseEvent<HTMLButtonElement>) => {
         e.preventDefault();
-        console.log("follow");
+        if (user.data.following == undefined) return;
+        const action = user.actions.find(
+            (value) =>
+                value.type ==
+                (user.data.following ? "unfollowUser" : "followUser")
+        );
+        if (!action || !action.link) return;
+
+        try {
+            await fetch(action.link, {
+                method: "POST"
+            });
+            router.refresh();
+        } catch (error) {
+            ToastMessage(String(error));
+        }
+        return;
     };
 
     return (
         <section className=" flex h-32 w-80 select-none gap-4">
             <div className="relative h-32 w-32 overflow-hidden rounded-full">
                 <Image
-                    src={profile.image}
+                    src={user.data.image}
                     alt="I"
                     layout="fill"
                     priority={true}
                 />
             </div>
             <div className=" flex flex-col justify-between">
-                <div className="flex">@{profile.name}</div>
+                <div className="flex">@{user.data.name}</div>
                 <div className="flex">
                     {session && (
                         <Button
-                            onClick={isOWner ? handleClick : handleFollow}
+                            onClick={isOWner ? handleEdit : handleFollow}
                             className=" w-32 rounded-full"
                         >
-                            {isOWner ? "Edit" : "Follow"}
+                            {isOWner
+                                ? "Edit"
+                                : user.data.following
+                                ? "Unfollow"
+                                : "Follow"}
                         </Button>
                     )}
                 </div>
                 <div className="flex items-center gap-2">
                     <Link
                         className="hover:text-accent-1"
-                        href={`/users/${profile.name}/followers`}
+                        href={`/users/${user.data.name}/followers`}
                     >
                         followers
                     </Link>
@@ -62,7 +88,7 @@ const CardProfile = ({
                     />
                     <Link
                         className="hover:text-accent-1"
-                        href={`/users/${profile.name}/follows`}
+                        href={`/users/${user.data.name}/follows`}
                     >
                         follows
                     </Link>
