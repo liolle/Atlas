639491ab33713a65a0d90af5a46f8227f127eff5
liolle <laurentlionel20@hotmail.com>
2023-10-30 23:30:54 +0100

    [Update] home - post feed v1

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,12 +1,62 @@
-import TestToast from "@/src/components/client/Buttons/TestToast";
+import PostDialog from "@/src/components/client/Buttons/PostDialog";
+import InfiniteScrollPosts from "@/src/components/client/area/infinitScroll";
 import BaseLayout from "@/src/components/server/BaseLayout";
-import React from "react";
+import { GetPosts } from "@/src/db/portal";
+import { APIResponse, APIVersion, LinkAction, PostType } from "@/src/types";
+import { getServerSession } from "next-auth";
+import React, { Suspense } from "react";
 
 export default async function Home() {
+    const session = await getServerSession();
+
+    const baseResponse: APIResponse = {
+        self: "",
+        version: APIVersion
+    };
+
+    const apiData = await GetPosts({
+        input: {
+            self: session?.user?.name || " ",
+            field: "all",
+            value: ""
+        },
+        APIResponse: baseResponse,
+        options: {
+            pagination: {
+                index: 1,
+                limit: 5
+            }
+        }
+    });
+
+    const posts = apiData.data?.content as unknown as {
+        actions: LinkAction[];
+        item: PostType;
+    }[];
+
     return (
         <BaseLayout>
-            <section className=" flex h-full items-center justify-center">
-                <TestToast />
+            <section className="  flex h-full w-full gap-4  @container">
+                <div className="flex h-full w-full overflow-y-auto">
+                    <main className="  flex h-fit min-h-screen  flex-[3_1_0] flex-col border-r-2 border-accent-2  ">
+                        <div className=" sticky left-0 top-0 z-10 flex h-[70px] w-[100%] items-center  justify-between px-4 backdrop-blur-sm ">
+                            <div></div>
+                            <PostDialog />
+                        </div>
+
+                        <Suspense fallback={<div>Loading</div>}>
+                            <InfiniteScrollPosts posts={posts || []} />
+                        </Suspense>
+                    </main>
+                    <div className=" hidden h-full max-w-[500px] flex-[1_1_0] flex-col  @[950px]:flex">
+                        <div className=" sticky left-0 top-0 z-10 flex h-[70px] w-[100%] items-center justify-center border-b-2 border-accent-2 bg-bgc  ">
+                            <span>WIP</span>
+                        </div>
+                        <div className=" flex h-full w-full flex-1 items-center justify-center ">
+                            <span>WIP</span>
+                        </div>
+                    </div>
+                </div>
             </section>
         </BaseLayout>
     );
