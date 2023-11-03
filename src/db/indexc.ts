a29ba20ca1ac8795ec1a4c14c5c8134442f1c8d3
liolle <laurentlionel20@hotmail.com>
2023-11-03 03:19:28 +0100

    [Fix] production - pulling db connection string from env

--- src/db/indexc.ts
+++ src/db/indexc.ts
@@ -2,11 +2,7 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import * as schema from "./schema.js";
 
-// const connectionString =
-//     process.env.DATABASE_URL ||
-//     "postgres:/postgres:postgres@localhost:5432/atlas";
-
-const connectionString = "postgres://postgres:postgres@localhost:5432/atlas";
+const connectionString = process.env.DATABASE_URL as string;
 
 const client = postgres(connectionString);
 
--- src/db/migrate.ts
+++ src/db/migrate.ts
@@ -2,8 +2,7 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import { migrate } from "drizzle-orm/postgres-js/migrator";
 import postgres from "postgres";
 
-// const connectionString = process.env.DATABASE_URL as string;
-const connectionString = "postgres://postgres:postgres@localhost:5432/atlas";
+const connectionString = process.env.DATABASE_URL as string;
 const sql = postgres(connectionString, { max: 1 });
 const db = drizzle(sql);
 
