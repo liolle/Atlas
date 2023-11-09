b2fe2cb9f818880e5563de07f407c40ab6997eff
liolle <laurentlionel20@hotmail.com>
2023-11-09 13:03:07 +0100

    [Update] aws services - added s3 object delete

---
+++ src/app/api/services/aws/s3/[action]/route.ts
@@ -0,0 +1,25 @@
+import UploadServiceServer from "@/src/services/aws/fileUpload";
+import { APIResponse, APIVersion } from "@/src/types";
+import { NextRequest, NextResponse } from "next/server";
+
+interface RouteContext {
+    params: { action: "objdelete" };
+}
+
+export async function POST(request: NextRequest, context: RouteContext) {
+    const baseResponse: APIResponse = {
+        self: request.nextUrl.href,
+        version: APIVersion
+    };
+
+    const key = request.nextUrl.searchParams.get("key");
+
+    if (context.params.action == "objdelete") {
+        if (!key) return NextResponse.json(baseResponse, { status: 400 });
+
+        await UploadServiceServer.deleteFile(key);
+        return NextResponse.json(baseResponse, { status: 200 });
+    }
+
+    return NextResponse.json(baseResponse, { status: 200 });
+}
