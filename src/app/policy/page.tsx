e53283a5ee7fec20aea2ccfac95213dfe2153069
liolle <laurentlionel20@hotmail.com>
2023-11-09 03:29:33 +0100

    [Update] policy theme

--- src/app/policy/page.tsx
+++ src/app/policy/page.tsx
@@ -3,14 +3,14 @@ import React from "react";
 
 export default async function Policy() {
     return (
-        <div className=" flex h-screen flex-col overflow-y-auto  ">
+        <div className=" flex h-screen flex-col overflow-y-auto text-content ">
             <div className=" sticky left-0 top-0 z-10 flex flex-[0_0_100px]   items-center justify-center border-b-2 border-accent-2 bg-bgc p-4">
                 <div className="  ">
                     <NavButton route="/home">Home</NavButton>
                 </div>
                 <span className=" p-2">Privacy Policy for Atlas </span>
             </div>
-            <div className=" flex h-full p-4">
+            <div className=" flex h-full p-4 ">
                 <div>
                     <div>
                         <div>
