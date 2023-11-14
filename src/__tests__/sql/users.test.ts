237816a0531e5d908c043e2d28d28a81feb66832
liolle <laurentlionel20@hotmail.com>
2023-11-14 23:33:18 +0100

    [Add] query mock tests - follow - like - users - posts

--- src/__tests__/sql/users.test.ts
+++ src/__tests__/sql/users.test.ts
@@ -1,60 +1,272 @@
-import { getAllUsers } from "@/src/db/sql/users/get";
-import { BaseError, UserType, isBaseError, isUserType } from "@/src/types";
-
-const mockUsers: UserType[] = [
-    {
-        id: "test-01",
-        name: "string",
-        email: "string",
-        image: "string",
-        created_at: new Date(),
-        following: true
-    }
-];
+import { GetUsers, UpdateUser } from "@/src/db/portal";
+import {
+    APIDataTypes,
+    APIMessage,
+    BaseError,
+    LinkAction,
+    UserType,
+    isBaseError,
+    isUserType
+} from "@/src/types";
 
 describe("Users", () => {
-    describe("getAllUsers", () => {
-        describe("Input: Undefined value & field != all", () => {
-            it("Should return an Error", async () => {
-                const response = await getAllUsers(
-                    {
+    describe("GetUsers", () => {
+        const mockUsers: UserType[] = [
+            {
+                id: "test-1",
+                name: "test-1",
+                email: "test@test.com",
+                image: "test-1",
+                created_at: new Date(),
+                following: true
+            }
+        ];
+        describe("Input: Incorrect", () => {
+            it("Undefined value & field != all : Should return an Error", async () => {
+                const response = await GetUsers({
+                    input: {
                         self: "test",
                         field: "id"
                     },
-                    {
-                        mocked: true,
-                        mockValue: mockUsers
+                    options: {
+                        mock: {
+                            mocked: true,
+                            mockValue: mockUsers
+                        }
                     }
-                );
+                });
 
-                const error = response as unknown as BaseError;
+                const error = response.error as unknown as BaseError;
 
                 expect(response).toBeDefined();
-                expect(isBaseError(response)).toEqual(true);
-                expect(error.error).toEqual("Incorrect input combination");
+                expect(response.error).toBeDefined();
+                expect(isBaseError(error)).toEqual(true);
+                expect(response.data).not.toBeDefined();
+                expect(response.rel).not.toBeDefined();
+                expect(error.error).toEqual("Empty string value");
+            });
+
+            it("Empty string value & field != all : Should return an Error", async () => {
+                const response = await GetUsers({
+                    input: {
+                        self: "test",
+                        field: "id",
+                        value: " "
+                    },
+                    options: {
+                        mock: {
+                            mocked: true,
+                            mockValue: mockUsers
+                        }
+                    }
+                });
+
+                const error = response.error as unknown as BaseError;
+
+                expect(response).toBeDefined();
+                expect(response.error).toBeDefined();
+                expect(isBaseError(error)).toEqual(true);
+                expect(response.data).not.toBeDefined();
+                expect(response.rel).not.toBeDefined();
+                expect(error.error).toEqual("Empty string value");
             });
         });
 
-        describe("Input: correct", () => {
-            it("Should return an array of user UserType[]", async () => {
-                const response = await getAllUsers(
-                    {
+        describe("Input: Correct", () => {
+            it("By field : id ", async () => {
+                const response = await GetUsers({
+                    input: {
                         self: "test",
                         field: "id",
-                        value: "test-01"
+                        value: "test-1"
                     },
-                    {
-                        mocked: true,
-                        mockValue: mockUsers
+                    options: {
+                        mock: {
+                            mocked: true,
+                            mockValue: mockUsers
+                        }
                     }
-                );
+                });
+                const users = response.data?.content as unknown as {
+                    actions: LinkAction[];
+                    item: APIDataTypes;
+                }[];
 
-                const users = response as unknown as UserType[];
+                // Expect value existence
+                expect(response).toBeDefined();
+                expect(response.data).toBeDefined();
+                expect(response.error).not.toBeDefined();
+                expect(response.data?.type).toEqual("user");
+                expect(response.data?.count).toEqual(1);
+                expect(isUserType(users[0].item)).toEqual(true);
+
+                // Expect output value
+                expect((users[0].item as UserType).id).toEqual("test-1");
+            });
+
+            it("By field : name ", async () => {
+                const response = await GetUsers({
+                    input: {
+                        self: "test",
+                        field: "name",
+                        value: "test-1"
+                    },
+                    options: {
+                        mock: {
+                            mocked: true,
+                            mockValue: mockUsers
+                        }
+                    }
+                });
+
+                const users = response.data?.content as unknown as {
+                    actions: LinkAction[];
+                    item: APIDataTypes;
+                }[];
+
+                // Expect value existence
+                expect(response).toBeDefined();
+                expect(response.data).toBeDefined();
+                expect(response.error).not.toBeDefined();
+                expect(response.data?.type).toEqual("user");
+                expect(response.data?.count).toEqual(1);
+                expect(isUserType(users[0].item)).toEqual(true);
+
+                // Expect output value
+                expect((users[0].item as UserType).name).toEqual("test-1");
+            });
+
+            it("By field : email ", async () => {
+                const response = await GetUsers({
+                    input: {
+                        self: "test",
+                        field: "email",
+                        value: "test@test.com"
+                    },
+                    options: {
+                        mock: {
+                            mocked: true,
+                            mockValue: mockUsers
+                        }
+                    }
+                });
+
+                const users = response.data?.content as unknown as {
+                    actions: LinkAction[];
+                    item: APIDataTypes;
+                }[];
+
+                // Expect value existence
+                expect(response).toBeDefined();
+                expect(response.data).toBeDefined();
+                expect(response.error).not.toBeDefined();
+                expect(response.data?.type).toEqual("user");
+                expect(response.data?.count).toEqual(1);
+                expect(isUserType(users[0].item)).toEqual(true);
+
+                // Expect output value
+                expect((users[0].item as UserType).name).toEqual("test-1");
+            });
+        });
+    });
+
+    describe("UpdateUser", () => {
+        const mockUserUpdate: APIMessage = {
+            type: "UpdateUser",
+            message: "Update successful"
+        };
+        describe("Input: Incorrect", () => {
+            it("Empty string value: Should return an Error", async () => {
+                const response = await UpdateUser({
+                    input: {
+                        field: "name",
+                        value: " ",
+                        email: "test@test.com"
+                    },
+                    options: {
+                        mock: {
+                            mocked: true,
+                            mockValue: mockUserUpdate
+                        }
+                    }
+                });
+
+                const error = response.error as unknown as BaseError;
 
                 expect(response).toBeDefined();
-                expect(users.length).toEqual(1);
-                expect(isUserType(users[0])).toEqual(true);
-                expect(users[0].id).toEqual("test-01");
+                expect(response.error).toBeDefined();
+                expect(isBaseError(error)).toEqual(true);
+                expect(response.data).not.toBeDefined();
+                expect(response.rel).not.toBeDefined();
+                expect(error.error).toEqual("Empty string value");
+            });
+
+            it("Wrong email format: Should return an Error", async () => {
+                const response = await UpdateUser({
+                    input: {
+                        field: "name",
+                        value: "test-2",
+                        email: "test@test"
+                    },
+                    options: {
+                        mock: {
+                            mocked: true,
+                            mockValue: mockUserUpdate
+                        }
+                    }
+                });
+
+                const error = response.error as unknown as BaseError;
+
+                expect(response).toBeDefined();
+                expect(response.error).toBeDefined();
+                expect(isBaseError(error)).toEqual(true);
+                expect(response.data).not.toBeDefined();
+                expect(response.rel).not.toBeDefined();
+                expect(error.error).toEqual("Invalid email format");
+            });
+        });
+
+        describe("Input: Correct", () => {
+            it("By field : name ", async () => {
+                const response = await UpdateUser({
+                    input: {
+                        field: "name",
+                        value: "test-2",
+                        email: "test@test.com"
+                    },
+                    options: {
+                        mock: {
+                            mocked: true,
+                            mockValue: mockUserUpdate
+                        }
+                    }
+                });
+
+                expect(response).toBeDefined();
+                expect(response.message).toBeDefined();
+                expect(response.message?.message).toEqual("Update successful");
+            });
+
+            it("By field : image ", async () => {
+                const response = await UpdateUser({
+                    input: {
+                        field: "image",
+                        value: "https://test-image.test/random/test.png",
+                        email: "test@test.com"
+                    },
+                    options: {
+                        mock: {
+                            mocked: true,
+                            mockValue: mockUserUpdate
+                        }
+                    }
+                });
+
+                expect(response).toBeDefined();
+                expect(response.message).toBeDefined();
+                expect(response.message?.type).toEqual("UpdateUser");
+                expect(response.message?.message).toEqual("Update successful");
             });
         });
     });
